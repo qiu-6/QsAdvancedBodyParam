b7bae690067b3ce4f4732a972b5809f275fee6c4
qiu-6 <qiu.dandelion@gmail.com>
2026-05-15 18:38:46 +0800

    add param for blood

--- src/main/java/net/qiu/advancedbodyparam/components/entity/blood/bloodComponentImpl.java
+++ src/main/java/net/qiu/advancedbodyparam/components/entity/blood/bloodComponentImpl.java
@@ -5,8 +5,10 @@ import net.minecraft.nbt.NbtCompound;
 public class bloodComponentImpl implements bloodComponent {
 
     private int blood = 20;
-
     private static final String bloodKey = "q_blood_level";
+
+    private int maxBlood = 60;
+
 
     @Override
     public void setBlood(int blood) {
