516e611adf1e22d06f1633cc048d883340baf6b9
qiu-6 <qiu.dandelion@gmail.com>
2026-05-16 20:23:34 +0800

    initiated

--- src/main/java/net/qiu/advancedbodyparam/QsAdvancedBodyParameters.java
+++ src/main/java/net/qiu/advancedbodyparam/QsAdvancedBodyParameters.java
@@ -2,16 +2,15 @@ package net.qiu.advancedbodyparam;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.qiu.advancedbodyparam.command.command;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class QsAdvancedBodyParameters implements ModInitializer {
 	public static final String MOD_ID = "qadvancedbodyparam";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String MOD_NAME = "Q's advanced body parameters";
 
 	@Override
 	public void onInitialize() {
@@ -19,6 +18,8 @@ public class QsAdvancedBodyParameters implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Registering " + MOD_NAME);
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> command.register(dispatcher));
 	}
 }
--- src/main/java/net/qiu/advancedbodyparam/QsAdvancedBodyParametersDataGenerator.java
+++ src/main/java/net/qiu/advancedbodyparam/QsAdvancedBodyParametersDataGenerator.java
@@ -2,10 +2,31 @@ package net.qiu.advancedbodyparam;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.entity.damage.DamageScaling;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+import net.qiu.advancedbodyparam.datagen.damageTypeProvider;
+import net.qiu.advancedbodyparam.datagen.languageProvider;
+import net.qiu.advancedbodyparam.util.tags.damageTypes;
 
 public class QsAdvancedBodyParametersDataGenerator implements DataGeneratorEntrypoint {
+
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		QsAdvancedBodyParameters.LOGGER.info("Datagen for language provider");
+		pack.addProvider(languageProvider::new);
+		QsAdvancedBodyParameters.LOGGER.info("Datagen for damage types provider");
+		pack.addProvider(damageTypeProvider::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+
+		registryBuilder.addRegistry(RegistryKeys.DAMAGE_TYPE, entries ->
+				entries.register(damageTypes.HEMORRHAGE, new DamageType("hemorrhage", DamageScaling.NEVER, 0)));
 	}
 }
