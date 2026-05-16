53b4cb6609d02304b4b329b5c362b120df67b1aa
qiu-6 <qiu.dandelion@gmail.com>
2026-05-16 20:23:06 +0800

    added datagen

---
+++ src/main/java/net/qiu/advancedbodyparam/datagen/damageTypeProvider.java
@@ -0,0 +1,28 @@
+package net.qiu.advancedbodyparam.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.entity.damage.DamageScaling;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.RegistryWrapper;
+import net.qiu.advancedbodyparam.util.tags.damageTypes;
+
+import java.util.concurrent.CompletableFuture;
+
+public class damageTypeProvider extends FabricDynamicRegistryProvider {
+
+    public damageTypeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup, Entries entries) {
+
+        entries.add(damageTypes.HEMORRHAGE, new DamageType("hemorrhage", DamageScaling.NEVER, 0));
+    }
+
+    @Override
+    public String getName() {
+        return "Damage Types Provider";
+    }
+}
---
+++ src/main/java/net/qiu/advancedbodyparam/datagen/languageProvider.java
@@ -0,0 +1,35 @@
+package net.qiu.advancedbodyparam.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+
+import static net.qiu.advancedbodyparam.QsAdvancedBodyParameters.MOD_ID;
+
+public class languageProvider extends FabricLanguageProvider {
+
+    public languageProvider(FabricDataOutput dataOutput) {
+        super(dataOutput);
+    }
+
+    private static String deathMessageKey(String path) {
+        return "death.attack." + path;
+    }
+
+    private static String commandKey(String path) {
+        return "commands." + MOD_ID + "." + path;
+    }
+
+    @Override
+    public void generateTranslations(TranslationBuilder translationBuilder) {
+
+        // Death message
+        translationBuilder.add(deathMessageKey("hemorrhage"), "%s bleed to death");
+
+        // Commands
+        translationBuilder.add(commandKey("sourceerror"), "This command can only be run by a player when no target is specified");
+        translationBuilder.add(commandKey("getblood"), "Blood level of %1$s is %2$d");
+        translationBuilder.add(commandKey("setblood"), "Blood level of %1$s is set to %2$d");
+        translationBuilder.add(commandKey("setmultipleblood"), "Blood level of %1$d players are set to %2$d");
+        translationBuilder.add(commandKey("componenterror"), "Could not find %1$s component on %2$s");
+    }
+}
