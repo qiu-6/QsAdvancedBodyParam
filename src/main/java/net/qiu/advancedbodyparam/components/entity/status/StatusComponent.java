11ba2b675b90c28f43cd08c4c4ea4f286673bc95
qiu-6 <qiu.dandelion@gmail.com>
2026-05-19 20:19:53 +0800

    add getters and setters

--- src/main/java/net/qiu/advancedbodyparam/components/entity/status/StatusComponent.java
+++ src/main/java/net/qiu/advancedbodyparam/components/entity/status/StatusComponent.java
@@ -1,6 +1,16 @@
 package net.qiu.advancedbodyparam.components.entity.status;
 
 import dev.onyxstudios.cca.api.v3.component.Component;
+import dev.onyxstudios.cca.api.v3.component.sync.AutoSyncedComponent;
+import dev.onyxstudios.cca.api.v3.component.tick.ServerTickingComponent;
+import net.qiu.advancedbodyparam.util.BodyParts;
+import net.qiu.advancedbodyparam.util.status.Status;
+import net.qiu.advancedbodyparam.util.status.StatusHelper;
 
-public interface StatusComponent extends Component {
+import java.util.HashSet;
+
+public interface StatusComponent extends Component, ServerTickingComponent, AutoSyncedComponent {
+
+    HashSet<StatusHelper> getStatusSet(BodyParts part);
+    void addStatus(BodyParts part, Status status, int duration, int intensity);
 }
