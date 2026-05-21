ed60fa2d783e3aa1f59c024a857d1abcac952a98
qiu-6 <qiu.dandelion@gmail.com>
2026-05-21 17:56:45 +0800

    add somemore config

--- src/main/java/net/qiu/advancedbodyparam/datagen/LanguageProvider.java
+++ src/main/java/net/qiu/advancedbodyparam/datagen/LanguageProvider.java
@@ -15,13 +15,17 @@ public class LanguageProvider extends FabricLanguageProvider {
     private static String deathMessageKey(String path) {
         return "death.attack." + path;
     }
-
     private static String commandKey(String path) {
         return "commands." + MOD_ID + "." + path;
     }
-
     private static String configKey(String path) {
         return MOD_ID + ".midnightconfig." + path;
+    }
+    private static String commandErrorKey(String path) {
+        return MOD_ID + ".commands.error." + path;
+    }
+    private static String disconnectKey(String path) {
+        return MOD_ID + ".disconnect." + path;
     }
 
     @Override
@@ -36,10 +40,26 @@ public class LanguageProvider extends FabricLanguageProvider {
         translationBuilder.add(commandKey("setblood"), "Blood level of %1$s is set to %2$d");
         translationBuilder.add(commandKey("setmultipleblood"), "Blood level of %1$d players are set to %2$d");
         translationBuilder.add(commandKey("componenterror"), "Could not find %1$s component on %2$s");
+        translationBuilder.add(commandKey("getstatusempty"), "There is no status on %1$s of %2$s");
+        translationBuilder.add(commandKey("getstatus"), "Status on %1$s of %2$s: ");
+        translationBuilder.add(commandKey("getstatuslist"), "Status: %1$s, Duration: %2$d, Intensity: %3$d");
+
+        // Disconnect player
+        translationBuilder.add(disconnectKey("no_component"), "You don't have %s component registered");
+
+        // Command error
+        translationBuilder.add(commandErrorKey("invalid_body_enum"), "Invalid body part %s");
 
         // Config
         translationBuilder.add(configKey("title"), MOD_NAME);
 
-        translationBuilder.add(configKey("fracturedComment"), "Bone fracture");
+        translationBuilder.add(configKey("disable_status_in_peaceful"), "Disable status in peaceful");
+
+        translationBuilder.add(configKey("fractured_comment"), "Bone fracture");
+
+        translationBuilder.add(configKey("fracture_fall_comment"), "Thresholds of falling to trigger potential bone fracture");
+        translationBuilder.add(configKey("mild_fall_threshold"), "Threshold to cause non-displaced fracture");
+        translationBuilder.add(configKey("mid_fall_threshold"), "Threshold to cause displaced fracture");
+        translationBuilder.add(configKey("severe_fall_threshold"), "Threshold to cause open fracture");
     }
 }
