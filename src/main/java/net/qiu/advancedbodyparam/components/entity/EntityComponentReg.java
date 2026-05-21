b8309165cafb0f1bafe848d71308afe9b6873dee
qiu-6 <qiu.dandelion@gmail.com>
2026-05-21 17:56:26 +0800

    register status component

--- src/main/java/net/qiu/advancedbodyparam/components/entity/EntityComponentReg.java
+++ src/main/java/net/qiu/advancedbodyparam/components/entity/EntityComponentReg.java
@@ -8,6 +8,8 @@ import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
 import net.minecraft.util.Identifier;
 import net.qiu.advancedbodyparam.components.entity.blood.BloodComponent;
 import net.qiu.advancedbodyparam.components.entity.blood.BloodComponentImpl;
+import net.qiu.advancedbodyparam.components.entity.status.StatusComponent;
+import net.qiu.advancedbodyparam.components.entity.status.StatusComponentImpl;
 
 import static net.qiu.advancedbodyparam.QsAdvancedBodyParameters.MOD_ID;
 
@@ -19,12 +21,24 @@ public class EntityComponentReg implements EntityComponentInitializer {
                     BloodComponent.class
             );
 
+    public static final ComponentKey<StatusComponent> STATUS_COMPONENT =
+            ComponentRegistry.getOrCreate(
+                    new Identifier(MOD_ID, "status"),
+                    StatusComponent.class
+            );
+
     @Override
     public void registerEntityComponentFactories(EntityComponentFactoryRegistry entityComponentFactoryRegistry) {
 
         entityComponentFactoryRegistry.registerForPlayers(
                 BLOOD_COMPONENT,
                 player -> new BloodComponentImpl(),
+                RespawnCopyStrategy.LOSSLESS_ONLY
+        );
+
+        entityComponentFactoryRegistry.registerForPlayers(
+                STATUS_COMPONENT,
+                StatusComponentImpl::new,
                 RespawnCopyStrategy.LOSSLESS_ONLY
         );
     }
